962b78126ab705826ef5d9951924b0d2d7691927
Mcguffinn <deronviledwin@gmail.com>
2021-04-26 22:57:03 -0400

    refactored code base and dry test api calls work as intended, styling is the next major sprint

--- weather.py
+++ weather.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 def get_user_latlng():
     # userLocation = userLocationData.state + userLocationData.city
-    userIp = geocoder.ip("me")  # <--- this works but userip doesnt?!
+    userIp = geocoder.ip("http://freegeoip.net/json")  # <--- this works but userip doesnt?!
     userLocation = userIp.latlng
     return userLocation
 
@@ -69,113 +69,6 @@ def build_params():
         "timezone": "America/New_York",
     }
 
-    fakeload = {
-        "data": {
-            "timelines": [
-                {
-                    "endTime": "2021-04-01T22:40:00-04:00",
-                    "intervals": [
-                        {
-                            "startTime": "2021-04-01T22:40:00-04:00",
-                            "values": {
-                                "precipitationIntensity": 0,
-                                "precipitationType": 1,
-                                "temperature": 59.22,
-                                "temperatureApparent": 59.22,
-                                "weatherCode": 1000,
-                                "windSpeed": 8.95,
-                            },
-                        }
-                    ],
-                    "startTime": "2021-04-01T22:40:00-04:00",
-                    "timestep": "current",
-                },
-                {
-                    "endTime": "2021-04-01T23:40:00-04:00",
-                    "intervals": [
-                        {
-                            "startTime": "2021-04-01T18:40:00-04:00",
-                            "values": {
-                                "precipitationIntensity": 0,
-                                "precipitationType": 1,
-                                "temperature": 64.06,
-                                "temperatureApparent": 64.06,
-                                "weatherCode": 1100,
-                                "windSpeed": 10.35,
-                            },
-                        },
-                        {
-                            "startTime": "2021-04-01T19:40:00-04:00",
-                            "values": {
-                                "precipitationIntensity": 0,
-                                "precipitationType": 1,
-                                "temperature": 62.04,
-                                "temperatureApparent": 62.04,
-                                "weatherCode": 1100,
-                                "windSpeed": 9.93,
-                            },
-                        },
-                        {
-                            "startTime": "2021-04-01T20:40:00-04:00",
-                            "values": {
-                                "precipitationIntensity": 0,
-                                "precipitationType": 1,
-                                "temperature": 61.14,
-                                "temperatureApparent": 61.14,
-                                "weatherCode": 1100,
-                                "windSpeed": 7.97,
-                            },
-                        },
-                        {
-                            "startTime": "2021-04-01T21:40:00-04:00",
-                            "values": {
-                                "precipitationIntensity": 0,
-                                "precipitationType": 1,
-                                "temperature": 59.79,
-                                "temperatureApparent": 59.79,
-                                "weatherCode": 1000,
-                                "windSpeed": 7.69,
-                            },
-                        },
-                        {
-                            "startTime": "2021-04-01T22:40:00-04:00",
-                            "values": {
-                                "precipitationIntensity": 0,
-                                "precipitationType": 1,
-                                "temperature": 59.22,
-                                "temperatureApparent": 59.22,
-                                "weatherCode": 1000,
-                                "windSpeed": 8.95,
-                            },
-                        },
-                        {
-                            "startTime": "2021-04-01T23:40:00-04:00",
-                            "values": {
-                                "precipitationIntensity": 0,
-                                "precipitationType": 1,
-                                "temperature": 56.52,
-                                "temperatureApparent": 56.52,
-                                "weatherCode": 1000,
-                                "windSpeed": 7.54,
-                            },
-                        },
-                    ],
-                    "startTime": "2021-04-01T18:40:00-04:00",
-                    "timestep": "1h",
-                },
-            ]
-        },
-        "warnings": [
-            {
-                "code": 246009,
-                "message": "The timestep is not supported in full for the time "
-                "range requested.",
-                "meta": {"from": "now", "timestep": "current", "to": "+1m"},
-                "type": "Missing Time Range",
-            }
-        ],
-    }
-
     return payload
 
 
@@ -197,7 +90,7 @@ def get_user_weather():
                                 "precipitationType": 1,
                                 "temperature": 59.22,
                                 "temperatureApparent": 59.22,
-                                "weatherCode": 1000,
+                                "weatherCode": 5100,
                                 "windSpeed": 8.95,
                             },
                         }
@@ -293,4 +186,21 @@ def get_user_weather():
     return fakeload
 
 
-debug(get_user_weather())
+# debug(get_user_weather())
+
+# def get_svg():
+#     currentWeather = get_user_weather()
+#     finalIcon = ''
+    
+    
+#     key = currentWeather["data"]["timelines"][0]["intervals"][0]["values"]["weatherCode"]
+    
+
+#     if key in weatherCodes.keys():
+#         svg = open(weatherCodes[key][1]).read()
+#         print(weatherCodes[key])
+    
+#     # svgFile= open('./icons/color/{}')
+#     return (svg,key )
+
+# debug(get_svg())
--- weatherly.py
+++ weatherly.py
@@ -1,17 +1,80 @@
-from flask import Flask, render_template, Response
-import logging
+from flask import Flask, render_template, Response, Markup, url_for, flash, jsonify, request
 from weather import get_user_weather
+from spoti import SpotifyAPI
+import os 
+import logging
+import json
+
 
 app = Flask(__name__)
 
+app.secret_key = "music"
+spotify = SpotifyAPI(client_id = os.environ.get("CLIENT_ID"), client_secret = os.environ.get("CLIENT_SECERET"))
+
+def call_user_weather():
+    currentWeather = get_user_weather()
+    weatherFolder = os.path.join('static', 'icons')
+
+def get_weather_status():
+    weatherCodes = {
+        4201: ["Heavy Rain", "static\icons\rain_heavy.svg"],
+        4001: ["Rain", "static\icons\rain.svg"],
+        4200: ["Light Rain", "static\icons\rain_light.svg"],
+        6201: ["Heavy Freezing Rain", "static\icons\freezing_rain_heavy.svg"],
+        6001: ["Freezing Rain",	"static\icons\freezing_rain.svg"],
+        6200: ["Light Freezing Rain",	"static\icons\freezing_rain_light.svg"],
+        6000:["Freezing Drizzle",	"static\icons\freezing_drizzle.svg"],
+        4000:["Drizzle"	"static\icons\drizzle.svg"],
+        7101:["Heavy Ice Pellets",	"static\icons\ice_pellets_heavy.svg"],
+        7000:["Ice Pellets",	"static\icons\ice_pellets.svg"],
+        7102:["Light Ice Pellets",	"static\icons\ice_pellets_light.svg"],
+        5101:["Heavy Snow",	"static\icons\snow_heavy.svg"],
+        5000:["Snow",	"static\icons\snow.svg"],
+        5100:["Light Snow",	"static\icons\snow_light.svg"],
+        5001:["Flurries",	"static\icons\flurries.svg"],
+        8000:["Thunderstorm",	"static\icons\tstorm.svg"],
+        2100:["Light Fog",	"static\icons\fog_light.svg"],
+        2000:["Fog",	"static\icons\fog.svg"],
+        1001:["Cloudy",	"static\icons\cloudy.svg"],
+        1102:["Mostly Cloudy",	"static\icons\mostly_cloudy.svg"],
+        1101:["Partly Cloudy",	"static\icons\partly_cloudy_day.svg"],
+        1100:["Mostly Clear",	"static\icons\mostly_clear_day.svg"],
+        1000:["Clear", "static\icons\clear_day.svg",],
+    }
+    key = get_user_weather()["data"]["timelines"][0]["intervals"][0]["values"]["weatherCode"]
+    svg = weatherCodes[key][1]
+    weatherStatus = weatherCodes[key][0]  
+    return (svg, weatherStatus)
+
 
 @app.route("/")
+@app.route("/home", methods=['GET', 'POST'])
 def index():
-    currentWeather = get_user_weather()
-    return render_template("weather.html", entries=currentWeather["data"])
+
+    return render_template("base.html")
+
+@app.route('/playlist/<query>')
+def get_playlist_data(query):
+    data = spotify.search(query= query, search_type="playlist")
+    playlistData = []
+
+    for items in data['playlists']["items"]:
+        image = items["images"][0]["url"]
+        name = items["name"]
+        playlistData.append({"image":image,"name": name})
+    return playlistData
+
+@app.route('/playcast')
+def playcast():
+    playlist = get_playlist_data(get_weather_status()[1])
+    weatherSVG = get_weather_status()[0]
+    weatherStatus = get_weather_status()[1]
+    return render_template("playlist.html", playlistData = playlist, weatherSVG = weatherSVG, weatherStatus = weatherStatus)
+
 
 
 if __name__ == "__main__":
     port = 5000
     app.run(debug=True)
+    app.config['UPLOAD_FOLDER'] = weatherFolder
     logging.debug("Started server, site: " + "http://localhost:" + str(port))
