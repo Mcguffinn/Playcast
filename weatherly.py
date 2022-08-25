36d79fd8eba0f6ecc3c3f77a9cf74f10f7edbb8b
mcguffinn <deronviledwin@gmail.com>
2022-08-24 20:06:17 -0400

    changed proxy setting

--- weatherly.py
+++ weatherly.py
@@ -75,7 +75,7 @@ def get_weather_status():
         1000: ["Clear", "static\icons\clear_day.svg"],
     }
     ip = (
-        request.headers["X-Forwarded-For"]
+        request.headers["REMOTE_ADDR"]
         # request.remote_addr
         # if request.remote_addr != None
         # else os.environ.get("REMOTE_ADDR")
